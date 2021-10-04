e68f7e8b941235de52ca9f5ad3b82442a640b859
olliestyle <olliestyle1989@gmail.com>
2021-10-04 13:24:05 +0300

    2. Тестовое задание - проект "Pooh JMS" [#268841 #210549].111

--- src/main/java/ru/job4j/pooh/QueueService.java
+++ src/main/java/ru/job4j/pooh/QueueService.java
@@ -13,16 +13,17 @@ public class QueueService implements Service {
         int status = 200;
 
         if ("GET".equals(req.method())) {
+            ConcurrentLinkedQueue<String> queue = map.get(req.queue());
             if (map.isEmpty()) {
                 text = "There are no queues at all";
                 status = 404;
-            } else if (map.get(req.queue()) == null) {
+            } else if (queue == null) {
                 status = 404;
                 text = "There are no queue with name " + req.queue();
-            } else if (map.get(req.queue()).isEmpty()) {
+            } else if (queue.isEmpty()) {
                 text = "Queue: " + req.queue() + " is empty";
             } else {
-                text = map.get(req.queue()).poll();
+                text = queue.poll();
             }
         } else if ("POST".equals(req.method())) {
             map.putIfAbsent(req.queue(), new ConcurrentLinkedQueue<>());
